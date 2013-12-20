baf323af6df9bc77f508057c44992553b1c2fd98
Federico Moro <fmoro@activosconcursales.com>
2013-12-20 14:04:55 +0100

    storage model

--- storage/admin.py
+++ storage/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from storage.models import *
 # Register your models here.
+
+admin.site.register(Tag)
+admin.site.register(Storage)
+admin.site.register(Object)
+admin.site.register(Collection)
--- storage/models.py
+++ storage/models.py
@@ -1,25 +1,26 @@
 #from django.db import models
 from mongoengine import *
 import datetime
+from django_extensions.db.models import AutoSlugField
 
 # Create your models here.
 
-#class Tag(EmbeddedDocument):
-#    name = StringField(max_lengt=50, required=True)
+class Tag(EmbeddedDocument):
+    name = StringField(max_length=50, required=True)
+    values = ListField(StringField(max_length=200))
+
+#class Tag(Document):
+#    name = StringField(max_length=50, required=True)
 #    values = ListField(StringField(max_length=200))
 
-class Tag(Document):
-    name = StringField(max_lengt=50, required=True)
-    values = ListField(StringField(max_length=200))
-    
 class Storage(Document):
-    name = StringField(max_lengt=100, required=True, unique=True)
-    slug = SlugField(max_length=100)
+    name = StringField(max_length=100, required=True, unique=True)
+    slug = AutoSlugField(populate_from='name', unique=True)
     date = DateTimeField(default=datetime.datetime.now)
     date_modified = DateTimeField(default=datetime.datetime.now)
-    #tags = ListField(Tag)
-    tags = ListField(ReferenceField(Tag, reverse_delete_rule=mongoengine.PULL))
-    
+    tags = ListField(EmbeddedDocumentField(Tag))
+    #tags = ListField(ReferenceField(Tag, reverse_delete_rule=mongoengine.PULL))
+
     meta = {
         'allow_inheritance': True,
         'shard_key': ('date',),
@@ -31,4 +32,5 @@ class Object(Storage):
     file = FileField()
 
 class Collection(Storage):
-    objects = ListField(ReferenceField(Object, reverse_delete_rule=mongoengine.PULL))
+    objects = ListField(ReferenceField(Object, reverse_delete_rule=PULL))
+
